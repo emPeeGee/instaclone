7a8f4ca079ddf698775d6748738ee22c13f5d060
Mihail Popovici <Mihail.Popovic@cedacrinternational.md>
2020-06-07 13:20:43 +0300

    feat: Get photos from unsplash API and display

--- src/components/profile/ImageList.js
+++ src/components/profile/ImageList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import ImageCard from './ImageCard';
 import './ImageList.css';
 
 class ImageList extends React.Component {
@@ -10,10 +12,20 @@ class ImageList extends React.Component {
     }
 
     onSearchSubmit = async () => {
+        const response = await axios.get('https://api.unsplash.com/photos', {
+            params: {page: 1, per_page: 20},
+            headers: {
+                Authorization: 'Client-ID EqpguUI3vrKZAFR1P_kR60A755goVw2I0IKb_DkelUE'
+            }
+        }); 
+
+        this.setState({ images: response.data });
     }
 
     render() {
-        const imgs = [];
+        const imgs = this.state.images.map(img => {
+            return <ImageCard key={img.id} image={img} />
+        })
 
         return (
            <div className="container__list">
