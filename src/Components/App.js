d88f1e04f1e5aa64d3f9ae9b68e41f0945b0770b
Mihail Popovici <Mihail.Popovic@cedacrinternational.md>
2020-06-07 20:33:08 +0300

    fix: Typo in Profile folder, Rename it

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from './Navigation/Navigation';
-import Profile from './profile/Profile';
+import Profile from './Profile/Profile';
 import './App.css';
 
 class App extends React.Component {
