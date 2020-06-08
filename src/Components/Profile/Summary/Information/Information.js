4de52daafaa1a3296943035717db8c38624d0c97
Mihail Popovici <Mihail.Popovic@cedacrinternational.md>
2020-06-08 11:32:28 +0300

    feature: Make Summary responsive

--- src/Components/Profile/Summary/Information/Information.js
+++ src/Components/Profile/Summary/Information/Information.js
@@ -6,11 +6,35 @@ import Stats from "./Stats/Stats";
 import Description from "./Description/Description";
 
 class Information extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.mediaQuery = {
+            tablet: 768,
+        };
+
+        this.state = {
+            windowWidth: null
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({windowWidth: document.body.clientWidth})
+        });
+    }
+
     render() {
+        let stats = <Stats />
+        if (this.state.windowWidth < this.mediaQuery.tablet) {
+            stats = <div></div>
+        }
+
         return(
             <div className="information">
                 <Overview />
-                <Stats />
+                {stats}
                 <Description />
             </div>
         );
--- src/Components/Profile/Summary/Summary.js
+++ src/Components/Profile/Summary/Summary.js
@@ -5,10 +5,36 @@ import Information from './Information/Information';
 import Stories from './Stories/Stories';
 
 import "./Summary.css";
+import Stats from "./Information/Stats/Stats";
 
 
 class Summary extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.mediaQuery = {
+            tablet: 768,
+        };
+
+        this.state = {
+            windowWidth: null
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({windowWidth: document.body.clientWidth})
+        });
+    }
+
+
     render() {
+        let stats = <div />
+        if (this.state.windowWidth < this.mediaQuery.tablet) {
+            stats = <Stats />
+        }
+
         return(
             <div className="summary">
                <div className="summary__dashboard">
@@ -18,6 +44,7 @@ class Summary extends React.Component {
 
                 <div>
                     <Stories />
+                    {stats}
                 </div>
             </div>
 
